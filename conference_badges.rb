b836a525dacf0b6c351b34c78c58baa9ae0354b8
Matt Etress <mattetress@me.com>
2018-10-04 21:03:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,5 +4,8 @@ end
 
 def batch_badge_creator(array)
   array.collect {|name| "Hello, my name is #{name}."}
+end
 
+def assign_rooms(array)
+  array.collect.with_index {|name, i| "Hello, #{name}! You'll be assigned to room #{i + 1}!"}
 end
